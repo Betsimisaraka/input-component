e9bfbc4c479d1efd9d7ffd74c492563b6655d447
Anita RASOA <anita.ras@onja.org>
2020-10-13 11:02:12 +0300

    Starting the project by creating the necessary js files and css file

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css';
+import Input from './Input'
+
+function App() {
+    return (
+        <article>
+            <h1>Inputs</h1>
+            <Input />
+        </article>
+    )
+}
+
+export default App
